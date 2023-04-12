da734a12e15d74de8f263316486ac90dc67718bb
kirilkin67 <kirilkin67@gmail.com>
2023-04-13 00:28:59 +0300

    lab 3

--- Loop/ex_01.py
+++ Loop/ex_01.py
@@ -1,18 +1,28 @@
-var = float(input("Введите значение переменной x: "))
-start = int(input("Введите начальное значение n: "))
-stop = int(input("Введите конечное значение n: "))
-step = int(input("Введите шаг функции: "))
-precision = int(input("Введите точность вычислений: "))
+try:
+    var = float(input("Введите значение переменной x: "))
+    if abs(var) <= 1:
+        print("Значение переменной по модулю должно быть больше 1")
+        exit()
+    # start = int(input("Введите начальное значение числового ряда: "))
+    stop = int(input("Введите конечное значение числового ряда: "))
+    step = int(input("Введите шаг функции: "))
+    precision = int(input("Введите точность вычислений: "))
 
-print("Таблица вычислений функции")
-cell = stop // 10 + 5
-print()
-result = 0
-counter = 1
-while start <= stop:
-    power = 2 * start + 1
-    result += 1 / (power * pow(var, power))
+    cell = 20
+    print("{:^{width}}".format("Таблица вычислений функции", width=cell*3))
+    print("{:^{width}}{:^{width}}{:^{width}}"
+          .format("Значение аргумента", "Значение функции", "Количество рядов", width=cell))
+    result = 0
+    counter = 1
+    start = 0
+    while start < stop:
+        power = 2 * start + 1
+        result += 1 / (power * pow(var, power))
 
-    print(f"{start:<{cell}d} {result:^0.{precision}f} {counter:>{cell}d}")
-    start += step
-    counter += 1
+        print(f"{start:^{cell}d}{result:^{cell}.{precision}f}{counter:^{cell}d}")
+        start += step
+        counter += 1
+    print(f"{'Значение аргумента':^{cell}}{'Значение функции':^{cell}}{'Количество рядов':^{cell}}")
+
+except ValueError:
+    print("Ошибка. Вы можете вводить только числа, состоящие из цифр, а не текст!!!")
